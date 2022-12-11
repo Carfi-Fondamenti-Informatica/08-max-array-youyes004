a9069b7eb7e3fb89bdcda500da5c5bf59583df19
Yousef El Sayed <115503073+youyes004@users.noreply.github.com>
2022-12-11 14:59:17 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,21 @@
+//
+// Created by Yousef El Sayed on 11/12/2022.
+//
+#include "lib.h"
+#include <iostream>
+using namespace std;
 
+float max(int a) {
+    float numeri [a];
+    float b= numeri [0];
+    for (int k=0; k<a ; k++)
+        numeri [k]=0;
+    for (int i=0; i<a ; i++ ) {
+        cin >> numeri [i];
+    }
+    for (int j = 0; j < a; j++) {
+        if (b < numeri[j])
+            b = numeri[j];
+    }
+    return b;
+}
