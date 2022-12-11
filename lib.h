c4ca1872edd3ac1b9af5eb0a07266f9a1901325b
Yousef El Sayed <115503073+youyes004@users.noreply.github.com>
2022-12-11 15:03:09 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,22 +1,8 @@
 //
 // Created by Yousef El Sayed on 11/12/2022.
 //
-#include "lib.h"
-#include <iostream>
-using namespace std;
 
-float max(int a) {
-    float numeri [a];
-    float b= numeri [0];
-    for (int k=0; k<a ; k++)
-        numeri [k]=0;
-    for (int i=0; i<a ; i++ ) {
-        cin >> numeri [i];
-    }
-    for (int j = 0; j < a; j++) {
-        if (b < numeri[j])
-            b = numeri[j];
-    }
-    return b;
-}
-
+#ifndef ESERCITAZIONE_8_LIB_H
+#define ESERCITAZIONE_8_LIB_H
+float max(int a);
+#endif //ESERCITAZIONE_8_LIB_H
